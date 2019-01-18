83f47ff48862e70ae008e91bca0e10328440fa06
Thomas Titscher <thomas.titscher@gmail.com>
2019-01-18 11:37:08 +0100

    Move import '..' hack to test_context.py

--- test/test_xdmf_fix.py
+++ test/test_xdmf_fix.py
@@ -1,34 +1,32 @@
-import os
-import sys
 import unittest
 import get_xdmf
-from dolfin import XDMFFile, Mesh, Measure, assemble
-
-sys.path.insert(0, os.path.abspath('..'))
+import test_context
 from xdmf_fix import fix
+
+from dolfin import XDMFFile, Mesh, Measure, assemble
 
 
 def area(xdmf):
     mesh = Mesh()
     with XDMFFile(xdmf) as f:
         f.read(mesh)
-    return assemble(1 * Measure('dx', mesh))
+    return assemble(1 * Measure("dx", mesh))
+
 
 class TestXdmfFix(unittest.TestCase):
-    
     def test_wrong_triangle_area(self):
         triangle = get_xdmf.triangle6()
         a = area(triangle)
-        self.assertNotAlmostEqual(a, 1.)
-   
+        self.assertNotAlmostEqual(a, 1.0)
+
     def test_wrong_tet_area(self):
         tet = fix.fix_tet10(get_xdmf.triangle6())
         a = area(tet)
-        self.assertNotAlmostEqual(a, 1.)
+        self.assertNotAlmostEqual(a, 1.0)
 
     def test_fix_tet10(self):
         tet = get_xdmf.tet10()
-        self.assertRaises(RuntimeError, area, tet) 
+        self.assertRaises(RuntimeError, area, tet)
 
         fix.fix_tet10(tet)
         area(tet)
