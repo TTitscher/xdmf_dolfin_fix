15501c30f2fce4de6af3f3df770948e6e8b7b051
Thomas Titscher <thomas.titscher@bam.de>
2019-01-20 21:42:28 +0100

    Add class XDMFMesh to handle the xdmf stuff
    Mainly:
     - copy of xdmf file including the h5 file
     - tetrahedron_10 --> tet_10
     - vertex sorting

---
+++ test/test_xdmf_mesh.py
@@ -0,0 +1,99 @@
+import os
+import unittest
+import tempfile
+
+from dolfin import XDMFFile, Mesh, Measure, assemble
+
+import test_context
+from xdmf_fix.xdmf_mesh import XDMFMesh
+from xdmf_fix import convert
+    
+tmp = tempfile.mkdtemp()
+    
+def _xdfm_from_geo_string(geo_string, name, dim):
+    xdmf = name + ".xdmf"
+    if os.path.exists(xdmf):
+        return xdmf
+
+    geo = os.path.join(tmp, name + ".geo")
+    msh = os.path.join(tmp, name + ".msh")
+
+    with open(geo, "w") as f:
+        f.write(geo_string)
+
+    convert.geo_to_msh(geo, msh, dim)
+    convert.msh_to_xdmf(msh, xdmf)
+    return xdmf
+
+
+def xdmf_triangle():
+    return _xdfm_from_geo_string(
+        """
+SetFactory("OpenCASCADE");
+Rectangle(1) = {0, 0, 0, 1, 1, 0};
+Physical Surface(1) = {1};
+            """,
+        "triangle6",
+        2,
+    )
+
+
+def xdmf_tet10():
+    return _xdfm_from_geo_string(
+        """
+SetFactory("OpenCASCADE");
+Box(1) = {0, 0, 0, 1, 1, 1};
+Physical Volume(1) = {1};
+            """,
+        "tet10",
+        3,
+    )
+
+def area(xdmf):
+    mesh = Mesh()
+    with XDMFFile(xdmf) as f:
+        f.read(mesh)
+    return assemble(1 * Measure("dx", mesh))
+
+def tmp_name(name, ext = ".xdmf"):
+    return os.path.join(tmp, name + ext)
+
+
+class TestXDMFMesh(unittest.TestCase):
+
+    def test_copy(self):
+        infile = xdmf_triangle()
+        out = tmp_name("copy")
+        out_h5 = tmp_name("copy", ".h5")
+
+        XDMFMesh(infile).copy(out)
+
+        self.assertTrue(os.path.exists(out))
+        self.assertTrue(os.path.exists(out_h5))
+
+    def test_triangle_area(self):
+        triangle = xdmf_triangle()
+        self.assertNotAlmostEqual(area(triangle), 1.0)
+
+        XDMFMesh(triangle).copy(tmp_name("tri")).fix_ordering()
+        self.assertAlmostEqual(area(tmp_name("tri")), 1.0)
+
+    def test_fix_tet10(self):
+        tet = xdmf_tet10()
+        self.assertRaises(RuntimeError, area, tet)
+
+        XDMFMesh(tet).copy(tmp_name("fix_tet10")).fix_tet10()
+        area(tmp_name("fix_tet10"))
+    
+    def test_tet_area(self):
+        tet = xdmf_tet10()
+        
+        XDMFMesh(tet).copy(tmp_name("fix_tet10")).fix_tet10()
+        self.assertNotAlmostEqual(area(tmp_name("fix_tet10")), 1.0)
+
+        XDMFMesh(tet).copy(tmp_name("tet")).fix_ordering()
+        self.assertAlmostEqual(area(tmp_name("tet")), 1.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ xdmf_fix/xdmf_mesh.py
@@ -0,0 +1,84 @@
+import os
+import re
+import shutil
+import logging
+import xml.etree.ElementTree as ET
+import h5py
+
+from . import node_reordering
+
+class XDMFMesh:
+    def __init__(self, xdmf):
+        self.xdmf = xdmf
+
+        topology_node = ET.parse(xdmf).find(".//Topology")
+        self.element_type = topology_node.attrib["TopologyType"].lower()
+
+        if self.topology_data_node().attrib["Format"].lower() != "hdf":
+            raise RuntimeError("Topology information must be in HDF format.")
+
+    def replace_string(self, search, replace):
+        with open(self.xdmf, "r") as f:
+            text = f.read()
+            text = re.sub(search, replace, text, flags=re.IGNORECASE)
+        with open(self.xdmf, "w") as f:
+            f.write(text)
+
+    def fix_tet10(self):
+        if self.element_type == "tetrahedron_10":
+            self.replace_string("tetrahedron_10", "tet_10")
+            self.element_type = "tet_10"
+
+    def topology_data_node(self):
+        return ET.parse(self.xdmf).find(".//Topology/DataItem")
+
+    def h5(self):
+        return self.topology_data_node().text.split(':')[0]
+
+    def topology_dataset(self):
+        return self.topology_data_node().text.split(':')[1]
+
+    def fix_ordering(self):
+        if self.element_type not in ["triangle_6", "tet_10", "tetrahedron_10"]:
+            return
+
+        self.fix_tet10()
+
+        dirname = os.path.dirname(self.xdmf)
+        h5 = os.path.join(dirname, self.h5())
+
+        with h5py.File(h5, "r+") as f:
+            data = f[self.topology_dataset()]
+            data_array = data[...]
+
+            if self.element_type == "triangle_6":
+                n_swaps = node_reordering.triangle6(data_array)
+            else:
+                n_swaps = node_reordering.tet10(data_array)
+
+            logging.info("Reordering required {} swaps.".format(n_swaps))
+
+            # we do not change the shape...
+            data[...] = data_array
+
+    def copy(self, dest):
+        """
+        dest ... xdmf file. Creates a h5 with same name.
+        """
+
+        # 1) copy xdmfs
+        shutil.copy(self.xdmf, dest)
+        xdmf_mesh_new = XDMFMesh(dest)
+
+        # 2) rename h5s in new xdmf mesh
+        dest_basename = os.path.basename(dest)
+        h5_dest_base = os.path.splitext(dest_basename)[0] + ".h5"
+        xdmf_mesh_new.replace_string(self.h5(), h5_dest_base)
+
+        # 3) copy hdfs
+        hdf_src = os.path.join(os.path.dirname(self.xdmf), self.h5())
+        hdf_dest = os.path.join(os.path.dirname(dest), h5_dest_base)
+        shutil.copy(hdf_src, hdf_dest)
+
+        return xdmf_mesh_new
+
